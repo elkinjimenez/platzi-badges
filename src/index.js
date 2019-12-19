18c062e7e35440a7fb215db80e376e5f2c0c1dc1
Elkin Omar Jimenez Garcia <elomjiga@hotmail.com>
2019-12-18 22:54:55 -0500

    Conociendo el entorno de react

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hola, Platzi Badges!';
-
 // const container = document.getElementById('app');
 
 // container.appendChild(element);
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
 
 //ESTO ES JSX -- SIEMPRE DEBE ESTAR IMPORTADO REACT
-const element = <h1>Hola, ¡Platzi Badges from React!</h1>
+// const element = <h1>Hola, ¡Platzi Badges from React!</h1>;
 
-const container = document.getElementById('app');
+// const element = document.createElement('h1');
+// element.innerText = 'Hola, Platzi Badges!';
 
-ReactDOM.render(element, container);
+var nombre = "elkin";
 
+// const element = React.createElement("h1", {}, `Hola, soy el hijo de ${nombre}`);
+
+const jsx = (
+  <div>
+    <h1>Hola, soy {nombre}</h1>
+    <h5>Y estudio en Platzi!</h5>
+  </div>
+);
+
+const container = document.getElementById("app");
+
+ReactDOM.render(jsx, container);
