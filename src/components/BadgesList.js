9b8ddf723f156cbffeab2a4f03d9c5ba67c78b18
Elkin Omar Jimenez Garcia <elomjiga@hotmail.com>
2019-12-21 20:44:58 -0500

    dando estilos a la lista

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -6,10 +6,23 @@ class BadgesList extends React.Component {
       <ul className="list-unstyled">
         {this.props.badges.map(badge => {
           return (
-            <li key={badge.id}>
-              <p>
-                {badge.firstName} {badge.lastName}
-              </p>
+            <li className="card my-3 p-3" key={badge.id}>
+              <div className="row">
+                <div className="col-2">
+                  <img
+                    className="rounded-circle img-fluid"
+                    src={badge.avatarUrl}
+                    alt="Avatar de usuario"
+                  />
+                </div>
+                <div className="col-10">
+                  <p className="my-0">
+                    {badge.firstName} {badge.lastName}
+                  </p>
+                  <p className="my-0">@{badge.twitter}</p>
+                  <p className="my-0">{badge.jobTitle}</p>
+                </div>
+              </div>
             </li>
           );
         })}
